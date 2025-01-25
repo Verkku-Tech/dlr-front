46f1c7d40c062154fcf433d02e831c6851c8b798
Saydenier Palacios <saydenier@gmail.com>
2025-01-25 10:51:13 -0400

    * Add product detail \n * Fix buy now button

--- src/app/shared/components/product-details-com/product-details-wrapper/product-details-wrapper.component.ts
+++ src/app/shared/components/product-details-com/product-details-wrapper/product-details-wrapper.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input } from "@angular/core";
+import { Component, inject, Input, OnInit } from "@angular/core";
 import { IProduct } from "../../../interfaces/product.interface";
 import { CartService } from "../../../services/cart.service";
 import { ProductService } from "../../../services/product.service";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+import { UtilsService } from "../../../services/utils.service";
 
 @Component({
   selector: 'app-product-details-wrapper',
   templateUrl: './product-details-wrapper.component.html',
   styleUrls: ['./product-details-wrapper.component.scss'],
 })
-export class ProductDetailsWrapperComponent {
+export class ProductDetailsWrapperComponent implements OnInit {
   @Input() product!: IProduct;
   @Input() isShowBottom: boolean = true;
+  private modalService = inject(NgbModal);
+  productModal!: NgbModalRef;
   textMore = false;
 
   handleTextToggle() {
@@ -20,7 +24,18 @@ export class ProductDetailsWrapperComponent {
   constructor(
     public productService: ProductService,
     public cartService: CartService
-  ) {}
+  ) {
+    this.modalService.activeInstances.subscribe((modal) => {
+      this.productModal = modal[0];
+    })
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    
+  }
+
+  close() {
+    this.productModal.close()
+    this.cartService.addCartProduct(this.product, this.productService.selectedVariant, this.productService.selectedStorage, this.cartService.orderQuantity, false)
+  }
 }
